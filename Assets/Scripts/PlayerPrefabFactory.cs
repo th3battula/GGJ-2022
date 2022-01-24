2af8668cecec2eee2f68e5ea9e023ff1ab6fae13
Tyler <tyler_batts@yahoo.com>
2022-01-23 22:34:13 -0500

    Added mirror and removed built-in networking and got basic scene working

--- Assets/Scripts/PlayerPrefabFactory.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerPrefabFactory : MonoBehaviour {
-    void OnEnable() {
-        this.transform.DetachChildren();
-    }
-}
